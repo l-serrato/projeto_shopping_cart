86e45ffe9dfac603603c246727bcb52593f01218
Luis <l-serrato@live.com>
2022-11-16 15:18:21 -0300

    requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,28 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function'); 
+  });
+
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('retorno da função fetchProduct com argumento é uma estrutura de dados igual ao objeto', async() => {
+    const results = await fetchProduct('MLB1405519561');
+    expect(results).toEqual(produto);
+  });
+
+  it('ao chamar a função sem argumento, retorna um erro com a mensagem', async() => {
+    const answer = await fetchProduct();
+    expect(answer).toEqual(new Error('ID não informado'));
   });
 });
