aac6fcae7f28e492b3cccbd804ea97843c3926f5
Luis <l-serrato@live.com>
2022-11-16 19:14:04 -0300

    2 lint

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,11 +1,11 @@
 export const fetchProduct = (QUERY) => {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
 };
 
 export const fetchProductsList = async (QUERY) => {
   if (!QUERY) throw new Error('Termo de busca não informado');
-  const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
+  const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
   const data = await resultado.json();
   return data.results;
 };
-//console.log(fetchProductsList('computador'));
+// console.log(fetchProductsList('computador'));
