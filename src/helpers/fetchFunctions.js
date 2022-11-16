27f323bf6a7a217506c4cd3c4437ea7a0b846cec
Luis <l-serrato@live.com>
2022-11-16 15:41:34 -0300

    requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,5 +3,8 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = () => {
-  // seu código aqui
+  if (!query.value) { throw Error('Termo de busca não informado'); }
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => data.results);
 };
