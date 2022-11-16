eb9e98a9fb282ea164bd1818778f484ff107e924
Luis <l-serrato@live.com>
2022-11-16 16:59:38 -0300

    2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,10 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async(query) => {
   if (!query.value) { throw Error('Termo de busca não informado'); }
-  const info = fetch('https://api.mercadolibre.com/sites/MLB/search?q=query')
-    .then((response) => response.json())
-    .then((data) => data.results);
+  const source = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const outcome = await fetch(source);
+  const info = await outcome.json();
   return info;
+    /* .then((response) => response.json())
+    .then((data) => data.results); */
 };
