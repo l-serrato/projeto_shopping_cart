a1b584357d54d7582d9f740fd70676288406df3a
Luis <l-serrato@live.com>
2022-11-16 16:17:05 -0300

    fix

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,7 +4,7 @@ export const fetchProduct = () => {
 
 export const fetchProductsList = () => {
   if (!query.value) { throw Error('Termo de busca não informado'); }
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY')
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=query')
     .then((response) => response.json())
     .then((data) => data.results);
 };
